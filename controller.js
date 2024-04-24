694d5d46ce993050482355a6c7cf6f41f0f44eab
ShailMurtaza <shailmurtaza@gmail.com>
2024-04-24 19:36:21 +0500

    Searching: update map with button

--- controller.js
+++ controller.js
@@ -48,7 +48,6 @@ function handle_mouse_down(event) {
     let node = select_node(event.clientX, event.clientY) // Get node under cursor
     let action = get_action() // Get current action
     if (node != null) {
-        node.color = COLORS[action] // Change color of node under cursor
         if (action == "START") {
              // If start node already exist then change its color to default node color
             if (map.start != null) {
@@ -65,6 +64,7 @@ function handle_mouse_down(event) {
             // Set map goal node
             map.goal = node
         }
+        node.color = COLORS[action] // Change color of node under cursor
     }
     if (action == "BLOCK") {
         canvas.addEventListener('mousemove', handle_mouse_move);
@@ -75,10 +75,16 @@ function handle_mouse_down(event) {
     }
 }
 
+function start() {}
+
 canvas.addEventListener('mousedown', handle_mouse_down);
 canvas.addEventListener('mouseup', ()=>{canvas.removeEventListener('mousemove', handle_mouse_move)});
 canvas.addEventListener('mouseout', ()=>{canvas.removeEventListener('mousemove', handle_mouse_move)});
 
+function update_map() {
+    map.length = parseInt(length_input.value)
+    map.create_nodes()
+}
 var map = new Map(ctx, length_input.value, space=5, rows=3, cols=4, line_width=1, stroke_color=COLORS["STROKE"], default_fill=COLORS["FILL"])
 map.create_nodes()
 animate()
