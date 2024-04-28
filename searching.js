127615112a7ba4caf4546bb68d1bfa15cb50e7c0
ShailMurtaza <shailmurtaza@gmail.com>
2024-04-28 16:47:31 +0500

    Searching: commenting Search class

--- searching.js
+++ searching.js
@@ -1,14 +1,17 @@
 class Search {
     constructor(map) {
-        this.map = map
-        this.algo = null
+        this.map = map // Map object
+        this.algo = null // Object of searching algorithm
     }
+    // start the search
     start() {
+        // If this.algo isn't null and interval isn't null then search is already running
         if (this.algo && this.algo.interval != null) {
             show_msg("Search already running")
         }
+        // Else if start and goal then start search
         else if (this.map.start && this.map.goal) {
-            this.algo = new algo[algo_select.value](this.map)
+            this.algo = new algo[algo_select.value](this.map) // Create new object of searching algorithm and pass map
             this.algo.start_search()
             update_status("RUNNING ...")
         }
@@ -16,8 +19,10 @@ class Search {
             show_msg("START or GOAL node not selected")
         }
     }
+    // stop the search
     stop() {
-        if (this.algo && this.algo.interval) {
+        // If this.algo exists and interval isn't null then cancel search by clearInterval
+        if (this.algo && this.algo.interval != null) {
             clearInterval(this.algo.interval)
             this.algo.interval = null
             show_msg("Search Stopped")
