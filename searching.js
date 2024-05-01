d07052fd028411b42ea6af56329cf9765f9ae252
ShailMurtaza <shailmurtaza@gmail.com>
2024-05-01 13:08:50 +0500

    Implemented A* searching algorithm

--- searching.js
+++ searching.js
@@ -418,11 +418,99 @@ class BestFS {
     }
 }
 
-const algo = {
-    0: DFS,
-    1: BFS,
-    2: Bi_directional,
-    3: BestFS,
+
+class A_star {
+    constructor(map) {
+        this.map = map
+        this.interval = null
+        this.queue = new Queue()
+        this.parent = {}
+        this.search = this.search.bind(this)
+        this.time = null
+    }
+
+    start_search() {
+        let start_node = this.map.start
+        let distance = map.distance(start_node, this.map.goal)
+        // Here priority/distance is (g_n + h_n). No need to calculate f(n) for start node as g_n is zero
+        this.queue.enqueue({node: start_node, priority: distance, g_n: 0})
+        this.parent[this.map.start.value] = null // set parent of start node as null
+        this.interval = setInterval(this.search, get_speed())
+        this.time = new Date()
+    }
+
+    search() {
+        // if queue is empty then clear interval
+        if (this.queue.queue.length == 0) {
+            clearInterval(this.interval)
+            this.interval = null
+            show_msg("Path Not Found")
+            update_status("Path Not Found")
+            return
+        }
+        else {
+            let node = this.queue.dequeue() // Pop first element from queue
+            // Store g(n)
+            let g_n = node.g_n
+            node = node.node
+            // Do not change color if node is start node
+            if (node.color != COLORS["START"])
+                node.color = COLORS["VISITED"]
+
+            // Get neighbors of node
+            let neighbors = this.map.get_neighbors(node.i, node.j)
+            for (let i=0;i<neighbors.length;i++) {
+                let n_node = neighbors[i]
+                if (n_node.color == COLORS["GOAL"]) {
+                    let delta_time = (new Date() - this.time) / 1000
+                    this.parent[n_node.value] = node.value
+                    clearInterval(this.interval)
+                    this.interval = null
+                    this.display_path()
+                    show_msg("Path Found!")
+                    show_msg(`TOOK: ${delta_time} sec`)
+                    update_status(`TIME: ${delta_time} sec`)
+                    return
+                }
+                if (n_node.color == COLORS["FILL"]) {
+                    this.parent[n_node.value] = node.value
+                    let distance = this.map.distance(n_node, this.map.goal)
+                    // From parent to child manhattan distance will always be 1 in our MAP.
+                    // Add 1 into g(n) of parent node to get g(n) of this child/neighbor node
+                    let n_node_g_n = g_n+1
+                    // Calculate f(n) = h(n) + g(n). And set it as priority
+                    this.queue.enqueue({node: n_node, priority: distance + n_node_g_n, g_n: n_node_g_n})
+                    n_node.color = COLORS["SELECTED"]
+                }
+            }
+            this.map.draw_nodes()
+        }
+    }
+
+    display_path() {
+        var path = []
+        var n = this.parent[this.map.goal.value]
+
+        // Create path array
+        while (n != null) { // n can also be 0. check for null
+            path.push(n)
+            n = this.parent[n]
+        }
+
+        // Change color of all selected nodes to visited for clear picture of this.map
+        while(this.queue.queue.length) {
+            let node = this.queue.dequeue().node
+            node.color = COLORS["VISITED"]
+        }
+        for (let i=0;i<path.length-1;i++) {
+            let row = Math.floor((path[i]) / this.map.cols) // Get row index using value of node
+            let col = Math.floor((path[i]) % this.map.cols) // Get col index using value of node
+            let node = this.map.nodes[row][col] // Get node using index values
+            node.color = COLORS["PATH"] // Set color of path
+        }
+        set_steps(path.length)
+        this.map.draw_nodes()
+    }
 }
 
 const search = new Search(map)
