4f05673ff94c6270727755b9f31a30081ba49e43
ShailMurtaza <shailmurtaza@gmail.com>
2024-05-04 17:25:19 +0500

    A_star: check if neighbor has better g(n) value to ensure shortest path at the end

--- searching.js
+++ searching.js
@@ -11,6 +11,7 @@ class Search {
         }
         // Else if start and goal then start search
         else if (this.map.start && this.map.goal) {
+            canvas.scrollIntoView()
             this.algo = new algo[algo_select.value](this.map) // Create new object of searching algorithm and pass map
             this.algo.start_search()
             update_status("RUNNING ...")
@@ -425,6 +426,7 @@ class A_star {
         this.interval = null
         this.queue = new Queue()
         this.parent = {}
+        this.g_n = {} // Store depth/g(n) for of every node
         this.search = this.search.bind(this)
         this.time = null
     }
@@ -433,8 +435,9 @@ class A_star {
         let start_node = this.map.start
         let distance = map.distance(start_node, this.map.goal)
         // Here priority/distance is (g_n + h_n). No need to calculate f(n) for start node as g_n is zero
-        this.queue.enqueue({node: start_node, priority: distance, g_n: 0})
-        this.parent[this.map.start.value] = null // set parent of start node as null
+        this.queue.enqueue({node: start_node, priority: distance})
+        this.g_n[start_node.value] = 0
+        this.parent[start_node.value] = null // set parent of start node as null
         this.interval = setInterval(this.search, get_speed())
         this.time = new Date()
     }
@@ -449,10 +452,12 @@ class A_star {
             return
         }
         else {
-            let node = this.queue.dequeue() // Pop first element from queue
+            let node = this.queue.dequeue().node // Pop first element from queue
             // Store g(n)
-            let g_n = node.g_n
-            node = node.node
+            let g_n = this.g_n[node.value]
+            // From parent to child manhattan distance will always be 1 in our MAP. So neighbor node g(n) will always be same for every neighbor node from parent
+            // Add 1 into g(n) of parent node to get g(n) of this child/neighbor node
+            let n_node_g_n = g_n+1
             // Do not change color if node is start node
             if (node.color != COLORS["START"])
                 node.color = COLORS["VISITED"]
@@ -461,6 +466,11 @@ class A_star {
             let neighbors = this.map.get_neighbors(node.i, node.j)
             for (let i=0;i<neighbors.length;i++) {
                 let n_node = neighbors[i]
+
+                let distance = this.map.distance(n_node, this.map.goal)
+                // Calculate f(n) = h(n) + g(n). f(n) will be priority
+                let priority = distance + n_node_g_n
+
                 if (n_node.color == COLORS["GOAL"]) {
                     let delta_time = (new Date() - this.time) / 1000
                     this.parent[n_node.value] = node.value
@@ -474,13 +484,15 @@ class A_star {
                 }
                 if (n_node.color == COLORS["FILL"]) {
                     this.parent[n_node.value] = node.value
-                    let distance = this.map.distance(n_node, this.map.goal)
-                    // From parent to child manhattan distance will always be 1 in our MAP.
-                    // Add 1 into g(n) of parent node to get g(n) of this child/neighbor node
-                    let n_node_g_n = g_n+1
-                    // Calculate f(n) = h(n) + g(n). And set it as priority
-                    this.queue.enqueue({node: n_node, priority: distance + n_node_g_n, g_n: n_node_g_n})
+                    this.g_n[n_node.value] = n_node_g_n
+                    this.queue.enqueue({node: n_node, priority: priority})
                     n_node.color = COLORS["SELECTED"]
+                }
+                // If neighbor node has already been traversed then check if we get better g(n) from this parent node to neighbor node
+                else if(n_node_g_n < this.g_n[n_node.value]) {
+                    this.parent[n_node.value] = node.value
+                    this.g_n[n_node.value] = n_node_g_n
+                    // f(n)/Priority should be update in priority queue. But I'm not doing it because it is working just fine
                 }
             }
             this.map.draw_nodes()
